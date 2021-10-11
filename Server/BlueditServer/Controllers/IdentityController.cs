048b9fff0ca5797119eb614f5f9f62f9b88902a0
Kostadin Kostadinov <kostadinovk2001@gmail.com>
2021-10-11 16:58:37 +0300

    Add server endpoint for registration

---
+++ Server/BlueditServer/Controllers/IdentityController.cs
@@ -0,0 +1,51 @@
+﻿using System.Threading.Tasks;
+using BlueditServer.Models.RequestModels.Identity;
+using BlueditServer.Services.Identity;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlueditServer.Controllers
+{
+    public class IdentityController : ApiController
+    {
+        private readonly IIdentityService identityService;
+
+        public IdentityController(IIdentityService service)
+        {
+            this.identityService = service;
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        [Route(nameof(Register))]
+        public async Task<IActionResult> Register(RegisterRequestModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest("Data is not valid");
+            }
+
+            var user = await identityService.RegisterAsync(model.Username, model.Password, model.ConfirmPassword);
+
+            if (user.Id == null)
+            {
+                return BadRequest("User cannot be registered please try again");
+            }
+
+            return Ok(user);
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        [Route(nameof(Login))]
+        public IActionResult Login(RegisterRequestModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest("Data is not valid");
+            }
+
+            return Ok("It is working...");
+        }
+    }
+}
