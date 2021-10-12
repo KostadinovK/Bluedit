7e98dec011773feda5f97b60b1509c7a9d8462e6
Kostadin Kostadinov <kostadinovk2001@gmail.com>
2021-10-12 16:20:18 +0300

    Removed the footer component

--- Client/src/app/shared/shared.module.ts
+++ Client/src/app/shared/shared.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SharedHeaderComponent } from './shared-header/shared-header.component';
-import { SharedFooterComponent } from './shared-footer/shared-footer.component';
 import {RouterModule} from '@angular/router';
 
 
@@ -9,11 +8,9 @@ import {RouterModule} from '@angular/router';
 @NgModule({
   declarations: [
     SharedHeaderComponent,
-    SharedFooterComponent
   ],
   exports: [
     SharedHeaderComponent,
-    SharedFooterComponent
   ],
     imports: [
         CommonModule,
